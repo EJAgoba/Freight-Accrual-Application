6845c3660df589fd7af429da211070930013a1ae
EJAgoba <154341459+EJAgoba@users.noreply.github.com>
2025-10-29 11:25:47 -0400

    Update exporters.py

--- exporters.py
+++ exporters.py
@@ -1,1 +1,8 @@
-
+import pandas as pd
+from io_utils import FileIO
+class Exporter:
+   """Wrapper for creating downloadable binaries."""
+   def export_full_excel(self, df: pd.DataFrame, sheet_name: str = "Re-Coded") -> bytes:
+       return FileIO.try_export_excel(df, sheet_name=sheet_name)
+   def export_csv(self, df: pd.DataFrame) -> bytes:
+       return df.to_csv(index=False).encode("utf-8")
