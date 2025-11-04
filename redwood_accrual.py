040685e61366cb93d39f6b0472a68e0b20434372
EJAgoba <154341459+EJAgoba@users.noreply.github.com>
2025-11-04 15:36:14 -0500

    Update redwood_accrual.py

--- redwood_accrual.py
+++ redwood_accrual.py
@@ -48,7 +48,7 @@ REVERT_MAP = {
 
 }
 
-BOL_CANDIDATES = ["BOL Number", "BOL", "Pro/BOL", "Pro / BOL", "Pro", "Reference", "Load Number", "Shipment #", Shipment#]
+BOL_CANDIDATES = ["BOL Number", "BOL", "Pro/BOL", "Pro / BOL", "Pro", "Reference", "Load Number", "Shipment #", "Shipment#"]
 
 REQUIRED_PIPELINE_COLS = [
 
